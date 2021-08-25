3ec7392be3bf76fa96d374d9a800b351e8248fcf
Jonas Góes <jonasdossantos1991@gmail.com>
2021-08-25 10:25:35 -0300

    commit inicial

---
+++ meetings-agenda-back/src/main/java/com/jonasdsg/meetingsagenda/meetingsagendaback/MeetingsAgendaBackApplication.java
@@ -0,0 +1,13 @@
+package com.jonasdsg.meetingsagenda.meetingsagendaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingsAgendaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingsAgendaBackApplication.class, args);
+	}
+
+}
---
+++ meetings-agenda-back/src/test/java/com/jonasdsg/meetingsagenda/meetingsagendaback/MeetingsAgendaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonasdsg.meetingsagenda.meetingsagendaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingsAgendaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
